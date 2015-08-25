600eb945c453c841bf3458f96da60de57d7777aa
shivtej1505 <15electronicmotor@gmail.com>
2015-08-25 20:15:11 +0530

    prompt completed

--- shell.c
+++ shell.c
@@ -3,7 +3,7 @@
 #include<unistd.h>
 #include<string.h>
 
-char *repHome(char *pwd, char *home)
+char *repHome(char *pwd,char *home)
 {
 	int lp,lh,i,min;
 	lp = strlen(pwd);
@@ -15,13 +15,14 @@ char *repHome(char *pwd, char *home)
 		if(pwd[i] != home[i])
 			break;
 	}
-	if(min == lh)
+	if(i == lh)
 	{
 		pwd[0]='~';
 		for(i = 1; i<=lp-lh; i++)
 			pwd[i] = pwd[i+lh-1];
 		pwd[i] = '\0';
-		printf("pwd: %s\n", pwd);
+		//printf("pwd: %s\n", pwd);
+		return pwd;
 	}
 	else
 		return pwd;
@@ -30,18 +31,17 @@ char *repHome(char *pwd, char *home)
 void promtPrint()
 {
 	char sysName[1005];
-	char *user, *pwd, *home,*tmp;
+	char *user=NULL, *pwd=NULL, *home=NULL,*tmp=NULL;
 	int res;
 
 	user = getenv("LOGNAME");
 	home = getenv("HOME");
 	pwd = repHome(getenv("PWD"), home);
 	res = gethostname(sysName, 1000);
-
-	printf("user:%s\n home:%s\n pwd:%s\n",user,home,sysName);
+	//printf("PWD: %s \n HOME: %s\n", pwd, home);
 
 	if(res == 0)
-		printf("LOL%s@%s:%s$ ",user, sysName, pwd);
+		printf("|SHELL|%s@%s:%s$ ",user, sysName, pwd);
 	else
 		// can't get system name
 		printf("An error occured.");
@@ -50,14 +50,31 @@ int main()
 {	
 	// The main shell loop
 	char c;
-	int noChars = 0;
+	char tmp[100005];
+	int noChars,flag;
 	while(1)
 	{
-		noChars++;
+		noChars=0;
+		flag=0;
 		promtPrint();
 		c = getc(stdin);
-//		if(c == '\n')
-			break;
+		while(c != '\n')
+		{
+			while(isspace(c))
+			{
+				flag=1;
+				c=getc(stdin);
+			}
+			if(flag)
+			{
+				tmp[noChars++]=' ';
+				flag=0;
+			}
+			tmp[noChars++]=c;
+			c = getc(stdin);
+		}
+		tmp[noChars]='\0';
+		printf("%s\n",tmp);
 	}
 	return 0;
 }
