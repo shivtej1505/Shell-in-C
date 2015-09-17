c7e142e8022202c44ef165054c5aaafa553c5cd9
shivtej1505 <15electronicmotor@gmail.com>
2015-09-17 21:43:54 +0530

    phase 1 done

---
+++ README.txt
@@ -0,0 +1,6 @@
+This is a shell written in c language.
+It can run basic commands like ls, cd , pwd ,echo.
+It will be improved to run background and foreground process.
+
+TO START SHELL:
+simply compile the shell.c file and execute it.
--- shell.c
+++ shell.c
@@ -3,6 +3,7 @@
 #include<unistd.h>
 #include<string.h>
 
+char *curComm[100];
 char *repHome(char *pwd,char *home)
 {
 	int lp,lh,i,min;
@@ -41,20 +42,126 @@ void promtPrint()
 	//printf("PWD: %s \n HOME: %s\n", pwd, home);
 
 	if(res == 0)
-		printf("|SHELL|%s@%s:%s$ ",user, sysName, pwd);
+		printf("%s@%s:%s$ ",user, sysName, pwd);
 	else
 		// can't get system name
 		printf("An error occured.");
 }
+
+void exe_commmand(int args)
+{
+	int i;
+	/*
+	   for(i=0;i<args;i++)
+	   printf("%d. %s\n",i,curComm[i]);
+	   */
+	if(!strcmp(curComm[0],"cd"))
+	{
+		if(args == 1)
+		{
+			int result = chdir(getenv("HOME"));
+			if(result == 0)
+				setenv("PWD",getenv("HOME"),1);
+		}
+		else if(args == 2)
+		{
+			int result = chdir(curComm[1]);
+			if(result == 0)
+				setenv("PWD",curComm[1],1);
+			else
+				printf("error.\n");
+		}
+		else
+		{
+			printf("cd command take 2 arguments. %d arguments given\n",args);
+		}
+	}
+	else
+	{
+		pid_t pidM;
+		pidM = fork();
+		if (pidM < 0)
+		{
+			printf("Cannot create child process.\n");
+			_exit(-1);
+		}
+		else if(pidM == 0)
+		{
+			execvp(curComm[0],curComm);
+			_exit(0);
+		}
+		wait();
+		return;
+	}
+}
+int parser(char *command,int size,int noSpace)
+{
+	printf("fine\n");
+	int i,j,k=0;
+	for(i=0;i<=noSpace;i++)
+	{
+		curComm[i] = malloc(100*sizeof(char));
+		if(k>size)
+		{
+			// May it happen
+			break;
+		}
+		j=0;
+		printf("%d\n",k);
+		while(command[k] != ' ' && command[k] != '\0')
+		{
+			curComm[i][j] = command[k];
+			j++;
+			k++;
+		}
+		if(command[k] == ' ' || command[k] == '\0')
+		{
+			curComm[i][j]='\0';
+			//		printf("%d. %s\n",i, curComm[i]);
+			k++;
+		}
+	}
+	for(;i<100;i++)
+		curComm[i]='\0';
+	return noSpace+1;
+}
+int calSpace(char *stg)
+{
+	int i,l=strlen(stg);
+	int space=0;
+	for(i=0;i<l;i++)
+	{
+		if(stg[i]==' ')
+			space++;
+	}
+	return space;
+}
+char *cutOffSpace(char *strg)
+{
+	int l=strlen(strg),i,j=0;
+	char sgt[1000];
+	for(i=0;i<l;i++)
+	{
+		if(strg[0] != ' ')
+			break;
+	}
+	for(;i<l;i++)
+		sgt[j++]=strg[i];
+	sgt[j]='\0';
+	return sgt;
+}
 int main()
 {	
 	// The main shell loop
+	int args;
 	char c;
-	char tmp[100005];
-	int noChars,flag;
+	char *tokens;
+	char tmp[1000005];
+	int noChars,flag,noSpace;
 	while(1)
 	{
 		noChars=0;
+		noSpace=0;
 		flag=0;
 		promtPrint();
 		c = getc(stdin);
@@ -62,11 +169,17 @@ int main()
 		{
 			while(isspace(c))
 			{
-				flag=1;
+				if(noChars)
+					flag=1;
 				c=getc(stdin);
+				if(c == '\n')
+					break;
 			}
+			if(c == '\n')
+				break;
 			if(flag)
 			{
+				noSpace++;
 				tmp[noChars++]=' ';
 				flag=0;
 			}
@@ -74,7 +187,18 @@ int main()
 			c = getc(stdin);
 		}
 		tmp[noChars]='\0';
-		printf("%s\n",tmp);
+		tokens = strtok(tmp,";");
+		while(tokens != NULL)
+		{
+			int lol = calSpace(tokens);
+			int len = strlen(tokens);
+			printf("%s %d %d\n",tokens,len,lol);
+			//args = parser(tokens, noChars,noSpace);
+			args = parser(cutOffSpace(tokens),len,lol);
+			exe_commmand(args);
+			tokens = strtok(NULL, ";");
+		}
+		//printf("%s\n",tmp);
 	}
 	return 0;
 }
